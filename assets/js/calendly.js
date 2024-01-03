4f86c3ae644b069fc5f29d5ebecf44c046b32b08
jstarr35 <jmstarr35@gmail.com>
2024-01-03 16:41:09 -0600

    site-update

---
+++ assets/js/calendly.js
@@ -0,0 +1,5 @@
+<!-- Calendly link widget begin -->
+<link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+<script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+<a href="" onclick="Calendly.initPopupWidget({url: 'https://calendly.com/jmstarr/introduction?background_color=222529&text_color=faeecf&primary_color=0ea0ff'});return false;">Schedule a Call</a>
+<!-- Calendly link widget end -->
